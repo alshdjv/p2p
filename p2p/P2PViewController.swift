75f2ed994a459f83f19a83bd3b9eed46c786b9e9
ToshiroSama <alishdjuraev@gmail.com>
2023-02-13 17:13:08 +0500

    middle part is finished. changed code structure

--- p2p/P2PViewController.swift
+++ p2p/P2PViewController.swift
@@ -5,24 +5,10 @@ struct Colors {
     static let greyCol = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
 }
 
-final class P2PViewController: UIViewController {
-    
-    private let headerLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 17, weight: .semibold)
-        label.text = "Перевод"
-        label.textColor = .black
-        return label
-    }()
-    
-    // TOP PART
-    private let headView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Colors.greyCol
-        view.layer.cornerRadius = 16.0
-        return view
-    }()
-    
+// MARK: - HeaderView
+
+final class HeaderView: UIView {
+
     private let cardIcon: UIImageView = {
         let image = UIImage(named: "cardImg")
         let imageView = UIImageView(image: image?.withRenderingMode(.alwaysOriginal))
@@ -40,34 +26,213 @@ final class P2PViewController: UIViewController {
         return textField
     }()
     
-    // MIDDLE PART
-    private let receiverView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Colors.greyCol
-        view.layer.cornerRadius = 20.0
-        return view
-    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setupLayer()
+        self.setupHeaderUI()
+    }
+    
+    private func setupLayer() {
+        self.backgroundColor = Colors.greyCol
+        self.layer.cornerRadius = 16.0
+    }
+    
+    private func setupHeaderUI() {
+        self.setSubviews()
+        self.setConstraints()
+    }
+    
+    private func setSubviews() {
+        self.addSubview(cardIcon)
+        self.addSubview(cardTextField)
+    }
+    
+    private func setConstraints() {
+        
+        cardIcon.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.snp.leading).offset(20)
+            make.bottom.equalTo(self.snp.bottom).offset(-16)
+            make.size.equalTo(CGSize(width: 24, height: 24))
+        }
+        
+        cardTextField.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(18)
+            make.leading.equalTo(self.cardIcon.snp.trailing).offset(14)
+            make.bottom.equalTo(self.snp.bottom).offset(-18)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - ReceiverView
+
+final class ReceiverView: UIView {
     
     private let receiverIcon: UIImageView = {
-        let image = UIImage(named: "friendImg")
+        let image = UIImage(named: "friendsImg")
         let imageView = UIImageView(image: image?.withRenderingMode(.alwaysOriginal))
         imageView.clipsToBounds = true
         return imageView
     }()
     
-    private let cardsView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Colors.greyCol
-        view.layer.cornerRadius = 20.0
-        return view
+    private let receiverLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Получатели"
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
     }()
     
-    private let buttonsStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .equalCentering
-        return stackView
+    private let receiverButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 20.0
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setupLayer()
+        self.setupReceiverUI()
+    }
+    
+    private func setupLayer() {
+        self.backgroundColor = Colors.greyCol
+        self.layer.cornerRadius = 20.0
+    }
+    
+    private func setupReceiverUI() {
+        self.setSubviews()
+        self.setConstraints()
+    }
+    
+    private func setSubviews() {
+        self.addSubview(receiverIcon)
+        self.addSubview(receiverLabel)
+        self.addSubview(receiverButton)
+    }
+    
+    private func setConstraints() {
+        
+        receiverIcon.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.snp.leading).offset(16)
+            make.size.equalTo(CGSize(width: 32, height: 32))
+        }
+        
+        receiverLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.receiverIcon.snp.bottom).offset(8)
+            make.leading.equalTo(self.receiverIcon.snp.leading)
+            make.bottom.equalTo(self.snp.bottom).offset(-16)
+        }
+        
+        receiverButton.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.snp.bottom)
+            make.width.equalTo(self.snp.width)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class TransferView: UIView {
+    
+    private let transferIcon: UIImageView = {
+        let image = UIImage(named: "arrows_round")
+        let imageView = UIImageView(image: image?.withRenderingMode(.alwaysOriginal))
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let transferLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Между картами"
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let transferButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 20.0
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setupLayer()
+        self.setupTransferUI()
+    }
+    
+    private func setupLayer() {
+        self.backgroundColor = Colors.greyCol
+        self.layer.cornerRadius = 20.0
+    }
+    
+    private func setupTransferUI() {
+        self.setSubviews()
+        self.setConstraints()
+    }
+    
+    private func setSubviews() {
+        self.addSubview(transferIcon)
+        self.addSubview(transferLabel)
+        self.addSubview(transferButton)
+    }
+    
+    private func setConstraints() {
+        
+        transferIcon.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top).offset(16)
+            make.leading.equalTo(self.snp.leading).offset(16)
+            make.size.equalTo(CGSize(width: 32, height: 32))
+        }
+        
+        transferLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.transferIcon.snp.bottom).offset(8)
+            make.leading.equalTo(self.transferIcon.snp.leading)
+            make.bottom.equalTo(self.snp.bottom).offset(-16)
+        }
+        
+        transferButton.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.snp.bottom)
+            make.width.equalTo(self.snp.width)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Main Controller
+
+final class P2PViewController: UIViewController {
+    
+    private let headView = HeaderView()
+    private let receiverView = ReceiverView()
+    private let transferView = TransferView()
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.text = "Перевод"
+        label.textColor = .black
+        return label
     }()
 
     override func viewDidLoad() {
@@ -85,14 +250,9 @@ final class P2PViewController: UIViewController {
     
     private func setSubviews() {
         self.view.addSubview(headerLabel)
-        
-        // Header
         self.view.addSubview(headView)
-        self.headView.addSubview(cardIcon)
-        self.headView.addSubview(cardTextField)
-        
-        // Middle
         self.view.addSubview(receiverView)
+        self.view.addSubview(transferView)
     }
     
     private func setConstraints() {
@@ -102,7 +262,6 @@ final class P2PViewController: UIViewController {
             make.centerX.equalTo(self.view.safeAreaLayoutGuide)
         }
         
-        // Header Part
         headView.snp.makeConstraints { make in
             make.top.equalTo(self.headerLabel.snp.bottom).offset(24)
             make.leading.equalTo(self.view.safeAreaLayoutGuide).offset(16)
@@ -110,20 +269,17 @@ final class P2PViewController: UIViewController {
             make.height.equalTo(56)
         }
         
-        cardIcon.snp.makeConstraints { make in
-            make.top.equalTo(self.headView.snp.top).offset(16)
-            make.leading.equalTo(self.headView.snp.leading).offset(20)
-            make.bottom.equalTo(self.headView.snp.bottom).offset(-16)
-            make.size.equalTo(CGSize(width: 24, height: 24))
+        receiverView.snp.makeConstraints { make in
+            make.top.equalTo(self.headView.snp.bottom).offset(16)
+            make.leading.equalTo(self.view.snp.leading).offset(16)
+            make.size.equalTo(CGSize(width: 163, height: 88))
         }
         
-        cardTextField.snp.makeConstraints { make in
-            make.top.equalTo(self.headView.snp.top).offset(18)
-            make.leading.equalTo(self.cardIcon.snp.trailing).offset(14)
-            make.bottom.equalTo(self.headView.snp.bottom).offset(-18)
+        transferView.snp.makeConstraints { make in
+            make.top.equalTo(self.headView.snp.bottom).offset(16)
+            make.trailing.equalTo(self.view.snp.trailing).offset(-16)
+            make.size.equalTo(CGSize(width: 163, height: 88))
         }
-        
-        // Middle Part
     }
     
     // MARK: - Methods
