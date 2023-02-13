7740364286db6e8e4f829196f0055faec5e7ba03
ToshiroSama <alishdjuraev@gmail.com>
2023-02-13 10:02:10 +0500

    headerLabel and initial func are configured

--- p2p/P2PViewController.swift
+++ p2p/P2PViewController.swift
@@ -2,11 +2,38 @@ import UIKit
 import SnapKit
 
 final class P2PViewController: UIViewController {
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.text = "Перевод"
+        label.textColor = .black
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemGray
+        self.view.backgroundColor = .systemBackground
         
+        self.setupUI()
+    }
+    
+    private func setupUI() {
+        self.setSubviews()
+        self.setConstraints()
+    }
+    
+    private func setSubviews() {
+        self.view.addSubview(headerLabel)
+    }
+    
+    private func setConstraints() {
+        
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(59)
+            make.leading.equalToSuperview().offset(148)
+            make.trailing.equalToSuperview().offset(148)
+        }
     }
 }
 
