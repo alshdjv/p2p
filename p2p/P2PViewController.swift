a361089cb4ca8e760c569934e0cf477bfc357678
ToshiroSama <alishdjuraev@gmail.com>
2023-02-13 09:27:33 +0500

    initial commit

---
+++ p2p/P2PViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class P2PViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemGray
+    }
+}
+
