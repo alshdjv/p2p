e99bebda41f29b01b1208994197e6a2dddf7d963
ToshiroSama <alishdjuraev@gmail.com>
2023-02-13 09:14:45 +0500

    Initial Commit

---
+++ p2p/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p2p
+//
+//  Created by Alisher Djuraev on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
